1a754334ca6286a885dbd006d99d1308cbc077d9
alvarezmartin3639 <60674744+alvarezmartin3639@users.noreply.github.com>
2023-09-09 15:31:13 -0300

    Subiendo ejercicio 1 de la clase 3.

---
+++ clase_3/Ejercicio_1/Bibloteca/Cuenta.cs
@@ -0,0 +1,56 @@
+﻿namespace Bibloteca
+{
+    public class Cuenta
+    {
+        private string titular;
+        private float cantidad;
+
+        public Cuenta(string titular, float cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string GetTitular()
+        {
+            return this.titular;
+        }
+
+        public float GetCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public string CuentaToString()
+        {
+            return $"Razón social: {this.GetTitular()} \nMonto actual: {this.GetCantidad()}";
+        }
+
+        public bool IngresarDinero(float montoParaIngresar)
+        {
+            bool seGeneroElIngreso = false;
+
+            if (montoParaIngresar > 0)
+            {
+                seGeneroElIngreso = true;
+                this.cantidad += montoParaIngresar;
+            }
+
+            return seGeneroElIngreso;
+        }
+
+        public bool RetirarDinero(float montoParaRetirar)
+        {
+            bool seGeneroElRetiro = false;
+
+            if (montoParaRetirar > 0 && montoParaRetirar <= this.cantidad)
+            {
+                seGeneroElRetiro = true;
+                this.cantidad -= montoParaRetirar;
+            }
+
+            return seGeneroElRetiro;
+        }
+    }
+
+}
