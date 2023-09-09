85b975ef853ed58d6c380f2965b6cd40ef167c3d
alvarezmartin3639 <60674744+alvarezmartin3639@users.noreply.github.com>
2023-09-09 15:18:53 -0300

    Subiendo ejercicio 2 de la clase 3.

---
+++ clase_3/Ejercicio_2/Bibloteca/Estudiante.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace Bibloteca
+{
+    public class Estudiante
+    {
+
+        #region Atributos
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+        #endregion
+
+        #region Constructores
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+        #endregion
+
+        #region Metodos
+        public void SetNotaPrimerParcial(int nota)
+        {
+            if (nota > 0 && nota < 11)
+            {
+                this.notaPrimerParcial = nota;
+            }
+        }
+
+        public void SetNotaSegundoParcial(int nota)
+        {
+            if (nota > 0 && nota < 11)
+            {
+                this.notaSegundoParcial = nota;
+            }
+        }
+
+        private float CalcularPromedio()
+        {
+            return (this.notaPrimerParcial + this.notaSegundoParcial) / 2;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            double nota = -1;
+
+            if (this.notaPrimerParcial > 3 && this.notaSegundoParcial > 3)
+            {
+                nota = Estudiante.random.Next(6, 10);
+            }
+
+            return nota;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            double notaFinal = this.CalcularNotaFinal();
+
+            sb.AppendLine($"Nombre -> {this.nombre} \nApellido -> {this.apellido} \nLegajo -> {this.legajo}");
+            sb.AppendLine($"Nota del primer parcial -> {this.notaPrimerParcial} \nNota del segundo parcial -> {this.notaSegundoParcial}");
+            sb.AppendLine($"Promedio de parciales -> {this.CalcularPromedio()}");
+            sb.AppendLine($"Nota Final -> {(notaFinal > -1 ? notaFinal.ToString() : "Alumno desaprobado")}");
+
+            return sb.ToString();
+        }
+        #endregion
+
+    }
+}
---
+++ clase_3/Ejercicio_2/Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Consola
+{
+    using Bibloteca;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante[] listaEstudiantes =
+                {
+                 new Estudiante("Perez", "1", "Juan"),
+                 new Estudiante("Flores", "2", "Roberto"),
+                 new Estudiante("Banquito", "3", "Esteban"),
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                listaEstudiantes[i].SetNotaPrimerParcial(i + 6);
+                listaEstudiantes[i].SetNotaSegundoParcial((i < 2) ? i + 7 : 2);
+                Console.WriteLine(listaEstudiantes[i].Mostrar());
+            }
+
+        }
+    }
+}
