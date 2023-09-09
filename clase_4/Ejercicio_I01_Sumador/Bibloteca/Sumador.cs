3ad48cce0a851a52572394e4d5e2b5a027f2b639
alvarezmartin3639 <60674744+alvarezmartin3639@users.noreply.github.com>
2023-09-09 17:53:04 -0300

    Finalizado el ejercicio I01_Sumador

---
+++ clase_4/Ejercicio_I01_Sumador/Bibloteca/Sumador.cs
@@ -0,0 +1,44 @@
+﻿namespace Bibloteca
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador():this(0)
+        {
+        }
+
+        public Sumador(int cantidadSumas)
+        {
+            this.CantidadSumas = cantidadSumas;
+        }
+
+        public int CantidadSumas { get => cantidadSumas; set => cantidadSumas = value; }
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+
+        public static explicit operator int(Sumador sumador) {
+                return sumador.CantidadSumas;
+        }    
+
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.CantidadSumas + s2.CantidadSumas; 
+        }
+
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            return (s1.CantidadSumas == s2.CantidadSumas) ;
+        }
+    }
+}
---
+++ clase_4/Ejercicio_I01_Sumador/Consola/Program.cs
@@ -0,0 +1,21 @@
+﻿using Bibloteca;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador1 = new Sumador();
+            Sumador sumador2 = new Sumador();
+
+            Console.WriteLine("Suma de numeros(sumador1): {0}", sumador1.Sumar(2,2));
+            Console.WriteLine("Suma de cadenas(sumador2): {0}", sumador2.Sumar("2", "2"));
+            Console.WriteLine("Cantidad de sumas utilizando el sumador1: {0}", (int)sumador1);
+            Console.WriteLine("Cantidad de sumas utilizando el sumador2: {0}", (int)sumador2);
+            Console.WriteLine("sumador1 + sumador2 = {0}", sumador1 + sumador2);
+            Console.WriteLine("Ambos sumadores hicieron la misma cantida de sumas?: {0}", (sumador1 | sumador2) ?"SI" :"NO");
+
+        }
+    }
+}
